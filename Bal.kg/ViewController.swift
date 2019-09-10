7e8bb5d915f32b57fb76ca7140aef1d2c98f88c1
Mairambek Abdrasulov <mairambek.abdrasulov@gmail.com>
2019-09-10 18:59:54 +0600

    first commit

---
+++ Bal.kg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bal.kg
+//
+//  Created by Mairambek on 10/09/2019.
+//  Copyright © 2019 Sunrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
